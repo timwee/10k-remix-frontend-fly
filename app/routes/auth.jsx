7c4508e0cd126933329905baea57375dbc920ffd
Tim Wee <tim.shawn@gmail.com>
2023-02-24 15:40:00 -0500

    Add logo

--- app/routes/auth.jsx
+++ app/routes/auth.jsx
@@ -10,6 +10,16 @@ export default function AuthPage() {
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            // className="mx-auto h-12 w-auto"
+            src="assets/logo.png"
+            alt="Logo"
+          />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Enter Access Code
+          </h2>
+        </div>
         <Form className="mt-8 space-y-6" method="post" id="auth-form">
           {actionData?.formError ? (
             <div className="rounded-md bg-red-50 p-4">
